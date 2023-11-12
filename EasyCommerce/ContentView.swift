2f045cc094bb9e092b5b0884fd26c221d1e1ba68
GichukiPaul <gichukipaul043@gmail.com>
2023-11-12 15:59:15 +0300

    SETUP: Init setup

---
+++ EasyCommerce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EasyCommerce
+//
+//  Created by user on 12/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EasyCommerce/EasyCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyCommerceApp.swift
+//  EasyCommerce
+//
+//  Created by user on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct EasyCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
